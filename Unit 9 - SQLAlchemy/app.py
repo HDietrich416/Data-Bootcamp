b992371468d313cfab457d477e163250d1400b9c
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2020-12-04 13:06:36 -0500

    .

--- Unit 9 - SQLAlchemy/app.py
+++ Unit 9 - SQLAlchemy/app.py
@@ -37,13 +37,14 @@ def precipitation():
                 .group_by(Measurement.date).all()
     session.close()
 
+    all_measurement = []
     for date, prcp in results:
         measurement_dict ={}
         measurement_dict['date'] = date
         measurement_dict['prcp'] = prcp
-        results.append(measurement_dict)
+        all_measurement.append(measurement_dict)
 
-    return jsonify(measurement_dict)
+    return jsonify(all_measurement)
 
 @app.route("/api/v1.0/stations")
 def stations(): 
@@ -54,11 +55,51 @@ def stations():
     return jsonify(results)
 
 
-#@app.route("/api/v1.0/tobs")
+@app.route("/api/v1.0/tobs")
+def tobs():
+    last_date = dt.datetime(2017, 8, 23)
+    first_date = last_date - timedelta(days=364)
+    session = Session(engine)
+    tobs_results = session.query(Measurement.date, Measurement.tobs)\
+                     .filter(Measurement.station == "USC00519281")\
+                    .filter(Measurement.date >=first_date).all()
+    session.close()
 
-#@app.route("/api/v1.0/<start>")
+    all_tobs = []
+    for date, tobs in tobs_results:
+        tobs_dict ={}
+        tobs_dict['date'] = date
+        tobs_dict['tobs'] = tobs
+        all_tobs.append(tobs_dict)
+
+    return jsonify(all_tobs)
+
+
+@app.route("/api/v1.0/<start>")
+def start():
+    session = Session(engine)
+    start_results = session.query( func.min(Measurement.tobs), \
+                    func.avg(Measurement.tobs), func.max(Measurement.tobs))\
+                    .filter(Measurement.date >=dt.datetime(start))\
+                    .order(Measurement.date).asc().all()
+    session.close()
+
+    all_start = []
+    for  min, avg, max in start_results:
+        start_dict ={}
+        min = start_dict[func.min('tobs')]
+        avg = start_dict[func.avg('tobs')]
+        max = start_dict[func.max('tobs')]
+        all_start.append(start_dict)
+
+    return jsonify(all_start)
+
+
 
 #@app.route("/api/v1.0/<start>/<end>")
+
+
+
 
 if __name__ =='__main__':
     app.run(debug=True)
