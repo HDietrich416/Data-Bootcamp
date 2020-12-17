bbb23eda095f6cba63ff0fafbf9d1e4452716881
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2020-12-17 14:23:05 -0500

    .

--- Unit 11 - Web Scraping/scrape_mars.py
+++ Unit 11 - Web Scraping/scrape_mars.py
@@ -48,12 +48,42 @@ def scrape():
 
     mars_table = mars_facts_df.to_html()
 
+    # Hemispheres
+
+    hem_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(hem_url)  
+
+    hem_html = browser.html
+    hem_soup = bs(hem_html,"html.parser")
+
+    results = hem_soup.find_all("div",class_='item')
+
+    image_urls = []
+    for result in results:
+            product_dict = {}
+            titles = result.find('h3').text
+            end_link = result.find("a")["href"]
+            image_link = "https://astrogeology.usgs.gov/" + end_link    
+            browser.visit(image_link)
+            html = browser.html
+            soup= bs(html, "html.parser")
+            downloads = soup.find("div", class_="downloads")
+            image_url = downloads.find("a")["href"]
+            product_dict['title']= titles
+            product_dict['image_url']= image_url
+            image_urls.append(product_dict)
+
+    # Add output data into dictionary
+
     mars_data = {
                 "nasa_title":nasa_title, 
                 "nasa_article":nasa_article, 
                 "featured_image_url":featured_image_url, 
-                "mars_table":mars_table
+                "mars_table":mars_table, 
+                "image_urls":image_urls, 
+                
                 }
+    
 
     # Close the browser
     browser.quit()
