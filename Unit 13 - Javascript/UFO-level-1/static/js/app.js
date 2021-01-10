467367a4a50fea49c6c2fc877ec82c8cd434a02c
Heather Dietrich <71471631+HDietrich416@users.noreply.github.com>
2021-01-10 16:34:45 -0500

    Update app.js

--- Unit 13 - Javascript/UFO-level-1/static/js/app.js
+++ Unit 13 - Javascript/UFO-level-1/static/js/app.js
@@ -1,4 +1,41 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Select the button
+
+var button = d3.select("#filter-btn")
+
+// Select the form
+
+var form = d3.select("form");
+
+// Create event handles
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // Select the input element 
+    var inputDate = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputDate.property("value");
+
+    // Filter data based on input date
+    var filteredData = tableData.filter(data => data.datetime === inputValue);
+
+    console.log(filteredData);
+
+    //var tableBody = d3.select("tbody")
+
+
+
+
+
+
+};
